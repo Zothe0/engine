8761424860c06f1f10b038c7d9146e5e9ad914c3
Avel111 <57135174+Avel111@users.noreply.github.com>
2021-03-10 13:41:30 +0300

    backup

--- src/main.go
+++ src/main.go
@@ -8,6 +8,8 @@ import (
 	"rpg/src/renderer"
 
 	"github.com/go-gl/gl/v4.6-core/gl"
+	"github.com/ungerik/go3d/mat4"
+	"github.com/ungerik/go3d/vec4"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -31,22 +33,22 @@ func main() {
 	}
 	shader := renderer.LoadShader("../res/shaders/defaultVertex.glsl", "../res/shaders/defaultFragment.glsl")
 	texCoords := []float32{
-		0, 1, // top left
 		0, 0, // bottom left
-		1, 0, // bottom right
+		0, 1, // top left
 		1, 1, // top right
+		1, 0, // bottom right
 		// 1, 1, // top right
 		// 0, 0, // bottom left
 	}
-	bytes, width, height := LoadImage("../res/textures/flint.png")
+	bytes, width, height := LoadImage("../res/textures/test1.png")
 	var texture uint32
 	gl.GenTextures(1, &texture)
 	gl.BindTexture(gl.TEXTURE_2D, texture)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(bytes))
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(width), int32(height), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(bytes))
 	gl.GenerateMipmap(gl.TEXTURE_2D)
 
 	vertices := []float32{
@@ -89,10 +91,13 @@ func main() {
 	fmt.Println("OpenGL version:", gl.GoStr(gl.GetString(gl.VERSION)))
 	fmt.Println("Maximum nr of vertex attributes supported: ", nrAttrbutes)
 
-	// projectionMatLocation := gl.GetUniformLocation(vdo1.Shader.ShaderProgram, gl.Str("projectionMat"+"\x00"))
-	// gl.UniformMatrix4f()
-	// projectionMat := mat4.Zero
-	// projectionMat.TranslateX(0.5)
+	var vecT *vec4.T = &vec4.T{1, 0, 0, 1}
+	transMat := &mat4.Ident
+	transMat = transMat.AssignZRotation(90)
+	transMat.Scale(0.5)
+	fmt.Println(vecT)
+	
+	gl.GetUniformLocation(shader.ShaderProgram, gl.Str("transform"+"/0x00"))
 
 	// gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
 	gl.ClearColor(0, 0, 0.3, 1)
@@ -117,7 +122,7 @@ func main() {
 }
 
 // LoadImage ...
-func LoadImage(path string) (pixels []byte, width, height int32) {
+func LoadImage(path string) (pixels []byte, w, h int) {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -128,12 +133,12 @@ func LoadImage(path string) (pixels []byte, width, height int32) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	width = int32(img.Bounds().Max.X)
-	height = int32(img.Bounds().Max.Y)
-	pixels = make([]byte, width*height*4)
+	w = img.Bounds().Max.X
+	h = img.Bounds().Max.Y
+	pixels = make([]byte, w*h*4)
 	bIndex := 0
-	for y := 0; y < int(height); y++ {
-		for x := 0; x < int(width); x++ {
+	for y := 0; y < int(h); y++ {
+		for x := 0; x < int(w); x++ {
 			r, g, b, a := img.At(x, y).RGBA()
 			pixels[bIndex] = byte(r / 256)
 			bIndex++
@@ -145,6 +150,19 @@ func LoadImage(path string) (pixels []byte, width, height int32) {
 			bIndex++
 		}
 	}
-
-	return pixels, width, height
+	if h%2 == 0 {
+		for i, j := w*h*2+1, (w*h*2+1)-w*4; i < (h*w*4+1)-w*4; i, j = i+w*4, j-w*4 {
+			for x := 0; x < int(w)*4; x++ {
+				pixels[i+x], pixels[j+x] = pixels[j+x], pixels[i+x]
+			}
+		}
+	} else {
+		middle := (h-1)*w*2 + 1
+		for i, j := middle+w*4, middle-w*4; i < (h*w*4+1)-w*4; i, j = i+w*4, j-w*4 {
+			for x := 0; x < int(w)*4; x++ {
+				pixels[i+x], pixels[j+x] = pixels[j+x], pixels[i+x]
+			}
+		}
+	}
+	return pixels, w, h
 }
