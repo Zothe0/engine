592286591a1bb86aeac51dc95839a8e9c5393695
Avel111 <57135174+Avel111@users.noreply.github.com>
2021-03-16 03:15:49 +0300

    change package name

--- src/manager/ResourceManager.go
+++ src/manager/ResourceManager.go
@@ -1,7 +1,7 @@
 package manager
 
 import (
-	"engine/src/renderer"
+	"engine/src/render"
 	"engine/src/utils"
 	"log"
 
@@ -67,23 +67,23 @@ var (
 
 // ResourceManager class
 type ResourceManager struct {
-	shadersMap  map[string]*renderer.Shader
-	texturesMap map[string]*renderer.Texture
-	spritesMap  map[string]*renderer.Sprite
+	shadersMap  map[string]*render.Shader
+	texturesMap map[string]*render.Texture
+	spritesMap  map[string]*render.Sprite
 }
 
 // InitResourceManager - constructor
 func InitResourceManager() (rm *ResourceManager) {
 	rm = new(ResourceManager)
-	rm.shadersMap = make(map[string]*renderer.Shader, utils.Sizeof(renderer.Shader{}))
-	rm.texturesMap = make(map[string]*renderer.Texture, utils.Sizeof(renderer.Texture{}))
-	rm.spritesMap = make(map[string]*renderer.Sprite, utils.Sizeof(renderer.Sprite{}))
+	rm.shadersMap = make(map[string]*render.Shader, utils.Sizeof(render.Shader{}))
+	rm.texturesMap = make(map[string]*render.Texture, utils.Sizeof(render.Texture{}))
+	rm.spritesMap = make(map[string]*render.Sprite, utils.Sizeof(render.Sprite{}))
 	return rm
 }
 func (r *ResourceManager) AddShader(name, vertexPath, fragmentPath string) {
-	r.shadersMap[name] = renderer.InitShader(vertexPath, fragmentPath)
+	r.shadersMap[name] = render.InitShader(vertexPath, fragmentPath)
 }
-func (r *ResourceManager) GetShader(name string) *renderer.Shader {
+func (r *ResourceManager) GetShader(name string) *render.Shader {
 	s := r.shadersMap[name]
 	if s == nil {
 		log.Fatal("Error: shader not found")
@@ -91,7 +91,7 @@ func (r *ResourceManager) GetShader(name string) *renderer.Shader {
 	return s
 }
 func (r *ResourceManager) AddTexture(name, path string, subTextures *[]string, subTexWidth, subTexHeight float32) {
-	r.texturesMap[name] = renderer.InitTexture(path, gl.LINEAR_MIPMAP_LINEAR, gl.LINEAR, gl.CLAMP_TO_EDGE)
+	r.texturesMap[name] = render.InitTexture(path, gl.LINEAR_MIPMAP_LINEAR, gl.LINEAR, gl.CLAMP_TO_EDGE)
 	texture := r.texturesMap[name]
 	xStep, yStep := subTexWidth/texture.Width, (subTexHeight / texture.Height)
 	var x, y float32
@@ -109,18 +109,18 @@ func (r *ResourceManager) AddTexture(name, path string, subTextures *[]string, s
 		}
 	}
 }
-func (r *ResourceManager) GetTexture(name string) *renderer.Texture {
+func (r *ResourceManager) GetTexture(name string) *render.Texture {
 	t := r.texturesMap[name]
 	if t == nil {
 		log.Fatal("Error: texture not found")
 	}
 	return t
 }
-func (r *ResourceManager) AddSprite(name, shader, texture, subTexture string) *renderer.Sprite {
-	r.spritesMap[name] = renderer.InitSprite(r.shadersMap[shader], r.texturesMap[texture], subTexture, &cubeVertices, &cubeColors, &cubeIndexes)
+func (r *ResourceManager) AddSprite(name, shader, texture, subTexture string) *render.Sprite {
+	r.spritesMap[name] = render.InitSprite(r.shadersMap[shader], r.texturesMap[texture], subTexture, &cubeVertices, &cubeColors, &cubeIndexes)
 	return r.spritesMap[name]
 }
-func (r *ResourceManager) GetSprite(name string) *renderer.Sprite {
+func (r *ResourceManager) GetSprite(name string) *render.Sprite {
 	s := r.spritesMap[name]
 	if s == nil {
 		log.Fatal("Error: shader not found")
--- src/render/VertexArray.go
+++ src/render/VertexArray.go
@@ -1,4 +1,4 @@
-package renderer
+package render
 
 import "github.com/go-gl/gl/v4.6-core/gl"
 
